3b1468cb019cae718cd91e6fa0693bfabe09f3ea
hyqdep <1016631056@qq.com>
2022-05-25 21:50:30 +0800

    second commit

--- src/main/java/git/testGit.java
+++ src/main/java/git/testGit.java
@@ -4,5 +4,6 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello1 git");
+        System.out.println("hello1 gitee");
     }
 }
